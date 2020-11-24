1424c067f78b7d2804988241234855aa30674994
wimblow1 <baptiste.le-goff@isen-ouest.yncrea.fr>
2020-11-24 16:31:06 +0100

    fonction get_question

--- Class/class_Bdd.py
+++ Class/class_Bdd.py
@@ -1,4 +1,6 @@
 import mysql.connector
+from class_QR import Questions
+
 
 class Bdd():
 
@@ -15,3 +17,21 @@ class Bdd():
     @classmethod
     def commit(cls):
         cls.bdd.commit()
+
+    @classmethod
+    def get_question(cls):
+        liste_question = []
+        query = "select id_question, libelle_question, nom_theme, difficulte_question from questions \
+                join theme on theme.id_theme = questions.id_theme"
+        
+        cursor.execute(query)
+        fetch = cursor.fetchall()
+        for row in fetch:
+            qid = str(row[0])
+            qlabel = str(row[1])
+            qtheme = str(row[2])
+            qlevel = int(row[3])
+        
+            question = Questions(qid, qlabel, qtheme, qlevel)
+            liste_question.append(question)
+        return liste_question
