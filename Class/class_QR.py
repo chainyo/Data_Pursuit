6552cd557483c5856917e539ccd3c8d2f7ee59e5
Thomas Chaigneau <thomas.chaigneau@isen-ouest.yncrea.fr>
2020-11-24 14:24:47 +0100

    ajout fonctionnalités et commentaires class questions

--- Class/class_QR.py
+++ Class/class_QR.py
@@ -1,15 +1,22 @@
 class Questions():
 
     def __init__(self, qlabel, qanswer, qtheme, qtype, qlevel):
+        # l'énoncé de la question
         self.label = qlabel
+        # la réponse associée
         self.answer = qanswer
+        # le thème de la question
         self.theme = qtheme
+        # le type de question (QCM, V/F, ouverte)
         self.type = qtype
+        # le niveau de difficulté
         self.level = qlevel
     
+    # Permer en utilisant print d'avoir un affichage console du thème et de l'énoncé de la question
     def __str__(self):
         return f"{self.theme}: {self.label} ?"
 
+    # Fonctionnalité pour comparer une string avec la réponse associée à la question
     def compare_answer(self, answer):
-        if answer.lower() == self.answer:
+        if str(answer.lower()) == self.answer:
             return True
