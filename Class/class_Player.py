a4aa42d5348ea71752708acc0b39bdd091b481cb
Thomas Chaigneau <thomas.chaigneau@isen-ouest.yncrea.fr>
2020-11-24 14:42:38 +0100

    ajout de fonctions à la classe joueur

--- Class/class_Player.py
+++ Class/class_Player.py
@@ -9,3 +9,14 @@ class Player():
         self.score = 0
         # Variable qui permet de dire si c'est son tour ou non
         self.turn = False
+    
+    def __str__(self):
+        return f"{self.name} à pour le moment {self.score} points."
+
+    # Pour récupérer nom du joueur
+    def give_name(self):
+        return str(self.name)
+    
+    # Pour récupérer score du joueur
+    def give_score(self):
+        return str(self.score)
