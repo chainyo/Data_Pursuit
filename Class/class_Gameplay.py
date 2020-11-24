d69409984402b154e95846ca14290bb0845c337a
Thomas Chaigneau <thomas.chaigneau@isen-ouest.yncrea.fr>
2020-11-24 14:36:40 +0100

    changement class interface pour gameplay (mécaniques)

---
+++ Class/class_Gameplay.py
@@ -0,0 +1,10 @@
+class Gameplay():
+
+    def __init__(self, nb_player):
+        self.nb_player = nb_player
+        
+        # Prévoir les différents temps de jeu
+
+        # Prévoir l'agencement des tours de jeu
+
+        # Prévoir l'enchainement Q/R, avancée etc...
