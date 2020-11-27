0776d7ef8dddb14410e88afccdb075c2cc18b195
ChainYo <thomas.chaigneau@isen-ouest.yncrea.fr>
2020-11-27 15:17:52 +0100

    ajout fonction comparaison réponses utilisateurs

--- Class/class_Gameplay.py
+++ Class/class_Gameplay.py
@@ -38,31 +38,58 @@ class Gameplay():
                 active_player.turn = True
                 # Tant que joueur.turn est vrai le joueur va pouvoir jouer (tant qu'il ne fait pas d'erreur)
                 while active_player.turn == True:
-                    self.ask_question()
-                    active_player.turn = False
+                    sucess = self.ask_question()
+                    if sucess == False:
+                        active_player.turn = False
     
     # fonction pour poser une question
     def ask_question(self):
         # fonction rand pour le choix de la question
         question = self.random_question()
+        # affichage du label de la question au joueur
         print(question)
+        # stockage des réponses possibles pour la question choisie
         reponses = self.get_question_answers(question.id)
-        self.show_reponses(len(reponses), reponses)
-        
+        # affichage des réponses pour le joueur
+        # et stockage de la réponse du joueur
+        rep = self.show_reponses(len(reponses), reponses)
+        # comparaison de la valeur de la réponse
+        sucess = self.rep_compare(rep, reponses)
+        return sucess
+
     # fonction random question
     def random_question(self):
         random.shuffle(self.questions)
         return self.questions[0]
 
+    # fonction pour obtenir les réponses liées à la question (id)
     def get_question_answers(self, qid):
         reponses = Bdd.get_answer(qid)
         return reponses
 
+    # fonction pour présenter les choix possibles liés à la question en fonction de la longueur de ses réponses
     def show_reponses(self, length, reponses):
+        # Question QCM
         if length == 3:
-            for i, rep in enumerate(reponses):
-                print(f"{i}. {rep}")
+            for rep in reponses:
+                print(f"- {rep}")
+        # Question Vrai/faux
         elif length == 2:
             for rep in reponses:
-                print(f"{i}. {rep}")
-        input("Ta réponse: ")
+                print(f"- {rep}")
+        # Dans tous les cas on veut un input du joueur
+        rep = input("Ta réponse: ")
+        return rep
+
+    # fonction pour comparer la réponse
+    def rep_compare(self, rep, li):
+        for obj in li:
+            if obj.label == rep:
+                if obj.value == str(1):
+                    print("Bonne réponse !")
+                    return True
+                else:
+                    pass
+            else:
+                print("Mauvaise réponse")
+                return False
