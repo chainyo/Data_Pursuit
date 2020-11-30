3cb4273fe3993ceb91746e8d85854302104cb23d
lefatoum2 <christian.mbarga-mvogo@isen-ouest.yncrea.fr>
2020-11-30 11:10:54 +0100

    Init changed

--- init.py
+++ init.py
@@ -1,18 +1,8 @@
 from Class.class_Bdd import Bdd
 from Class.class_Gameplay import Gameplay
-from Class.class_Interface import App
 
-# stockage des données
-questions_1, questions_2, questions_3 = Bdd.get_question_1(), Bdd.get_question_2(), Bdd.get_question_3()
-questions = (questions_1, questions_2, questions_3)
+# Stockage des données
+questions = Bdd.get_question()
 
-# stockage des différents thèmes
-themes = ("Big Data", "IA", "Ethique", "Python", "Mathematiques")
-
-# affichage de la fenêtre de l'appli
-#if __name__ == "__main__":
-#    app = App()
-#    app.mainloop()
-
-# création de la partie
-game = Gameplay(questions, themes)
+# Création de la partie
+game = Gameplay(questions)
