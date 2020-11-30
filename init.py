1265f4244a01553033bb79e142fed748c642d908
lefatoum2 <christian.mbarga-mvogo@isen-ouest.yncrea.fr>
2020-11-30 11:57:59 +0100

    Init changed

--- init.py
+++ init.py
@@ -1,5 +1,7 @@
 from Class.class_Bdd import Bdd
 from Class.class_Gameplay import Gameplay
+from Class.class_Interface import *
+
 
 # stockage des données
 questions_1, questions_2, questions_3 = Bdd.get_question_1(), Bdd.get_question_2(), Bdd.get_question_3()
